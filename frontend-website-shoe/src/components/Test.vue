24a5ec14ba7bfe347eddbeb833810dfaeae2f580
dinh.sans <ngocdinh2k1@gmail.com>
2023-03-07 22:37:06 +0700

    create init front end config

---
+++ frontend-website-shoe/src/components/Test.vue
@@ -0,0 +1,14 @@
+<template lang="">
+  <div class="bg-slate-500">{{ msg }}</div>
+</template>
+<script>
+export default {
+  name: 'test-vue',
+  data() {
+    return {
+      msg: 'Hello vue js'
+    }
+  }
+}
+</script>
+<style lang=""></style>
