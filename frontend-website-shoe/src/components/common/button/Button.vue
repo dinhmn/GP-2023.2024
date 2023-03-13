4dac97cda49feb168582965a41c2ff93a4602305
dinh.sans <ngocdinh2k1@gmail.com>
2023-03-13 23:29:19 +0700

    feature/frontend/#6: Add form login and logout of website version 1

---
+++ frontend-website-shoe/src/components/common/button/Button.vue
@@ -0,0 +1,25 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="block text-center min-w-[300px]">
+    <button class="w-full px-8 py-2 rounded" :class="{ className }" :id="{ name }">
+      {{ props.text }}
+    </button>
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  name: {
+    type: String,
+    default: 'id'
+  },
+  text: {
+    type: String,
+    default: 'Button'
+  },
+  className: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+<style lang=""></style>
---
+++ frontend-website-shoe/src/components/common/input/Input.vue
@@ -0,0 +1,24 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template lang="">
+  <div class="flex items-center justify-center w-full">
+    <label class="pr-2" :for="{ type }">{{ props.text }}</label>
+    <input class="w-full text-gray-800" :type="{ type }" :name="{ name }" :id="{ name }" value="" />
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'text'
+  },
+  name: {
+    type: String,
+    default: 'formInput'
+  },
+  text: {
+    type: String,
+    default: 'Input'
+  }
+})
+</script>
+<style lang=""></style>
