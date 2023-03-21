571a3f2e0a152309c3ff4e6883dc77f513e69326
dinh.sans <ngocdinh2k1@gmail.com>
2023-03-21 22:07:49 +0700

    feature/frontend/#34: Update detail product page of website

---
+++ frontend-website-shoe/src/components/common/CommonItem.vue
@@ -0,0 +1,21 @@
+<template lang="">
+  <div class="mt-2">
+    <h2 class="text-2xl font-bold">{{ props.title }}</h2>
+    <slot />
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    default: 'Mô tả sản phẩm'
+  }
+})
+</script>
+<style lang="css" scoped>
+h2 {
+  background-color: #ff4a684d;
+  padding: 8px 12px;
+  border-radius: 4px;
+}
+</style>
