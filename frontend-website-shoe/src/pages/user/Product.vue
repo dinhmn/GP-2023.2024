b14cea35ce9cd56e9fab22dd067d4d2427b6be24
dinh.sans <ngocdinh2k1@gmail.com>
2023-03-29 22:33:40 +0700

    feature/frontend/#54: Create page user product.

--- frontend-website-shoe/src/pages/user/Product.vue
+++ frontend-website-shoe/src/pages/user/Product.vue
@@ -5,22 +5,35 @@
     Heading
   </div>
   <div class="grid w-full grid-cols-4 gap-2">
-    <div class="col-span-1 form-search h-[100vh] bg-slate-200">
-      <h3>Danh mục sản phẩm</h3>
+    <div class="col-span-1 form-search h-[100%] bg-slate-700 rounded-md p-2">
+      <h3 class="text-[#17b1ea] font-bold text-xl block text-left">Danh mục sản phẩm</h3>
+      <div>
+        <ul>
+          <li>Danh mục</li>
+          <li>Danh mục</li>
+          <li>Danh mục</li>
+          <li>Danh mục</li>
+          <li>Danh mục</li>
+          <li>Danh mục</li>
+        </ul>
+      </div>
     </div>
     <div class="col-span-3">
       <div class="grid grid-cols-4 gap-3">
-        <Item
+      
+        <div v-for="item in count" classChild="col-span-1 mb-3 bg-[#0c3247] text-[#17b1ea]">
+          <router-link :to="{ name: 'DetailProduct', params: {id: item}}">
+            <Item
             :price="item * 16456 + 80000"
             :priceSale="item * 92340 + 70000"
             :quantitySold="40 + item * 3"
             productName="Giày Nike"
             src="item.png"
             alt="Giày"
-            v-for="item in count"
             :key="item"
-            classChild="col-span-1 mb-3 bg-[#0c3247] text-[#17b1ea]"
           />
+          </router-link>
+        </div>
       </div>
     </div>
   </div>
