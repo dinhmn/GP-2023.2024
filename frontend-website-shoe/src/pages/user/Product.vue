df7bd83a1aba57b0e51efb378bc3b72721c69620
Mai Ngoc Dinh <ngocdinh2k1@gmail.com>
2023-03-30 18:17:12 +0700

    feature/frontend/#54: Update page user product.

--- frontend-website-shoe/src/pages/user/Product.vue
+++ frontend-website-shoe/src/pages/user/Product.vue
@@ -6,21 +6,15 @@
   </div>
   <div class="grid w-full grid-cols-4 gap-2">
     <div class="col-span-1 form-search h-[100%] bg-slate-700 rounded-md p-2">
-      <h3 class="text-[#17b1ea] font-bold text-xl block text-left">Danh mục sản phẩm</h3>
+      <h3 class="text-[#17b1ea] font-bold text-xl block text-center py-2">Danh mục sản phẩm</h3>
       <div>
-        <ul>
-          <li>Danh mục</li>
-          <li>Danh mục</li>
-          <li>Danh mục</li>
-          <li>Danh mục</li>
-          <li>Danh mục</li>
-          <li>Danh mục</li>
+        <ul class="flex flex-col gap-2 text-sm">
+          <li v-for="item in count" :key="item" class="py-2 px-4 text-base text-white hover:bg-[#0c3247] hover:text-[#17b1ea] border-transparent border-l-[4px] hover:border-[#17b1ea] border-solid">Danh mục</li>
         </ul>
       </div>
     </div>
     <div class="col-span-3">
       <div class="grid grid-cols-4 gap-3">
-      
         <div v-for="item in count" classChild="col-span-1 mb-3 bg-[#0c3247] text-[#17b1ea]">
           <router-link :to="{ name: 'DetailProduct', params: {id: item}}">
             <Item
@@ -28,7 +22,7 @@
             :priceSale="item * 92340 + 70000"
             :quantitySold="40 + item * 3"
             productName="Giày Nike"
-            src="item.png"
+            src="default.png"
             alt="Giày"
             :key="item"
           />
@@ -46,4 +40,5 @@ import Item from '@/components/common/product/Item.vue';
 const count = 10
 
 </script>
-<style lang=""></style>
+<style lang="css">
+</style>
