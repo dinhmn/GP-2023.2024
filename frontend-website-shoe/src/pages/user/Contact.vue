60e85c1185990195cf83495fc72b676613479288
Mai Ngoc Dinh <ngocdinh2k1@gmail.com>
2023-03-30 19:52:42 +0700

    feature/frontend/#57: Update page contact.

--- frontend-website-shoe/src/pages/user/Contact.vue
+++ frontend-website-shoe/src/pages/user/Contact.vue
@@ -1,22 +1,27 @@
 <template lang="">
-  <BasePage class="flex flex-col items-center justify-center gap-5 text-white">
+  <BasePage class="flex flex-col items-center justify-center gap-5 text-white ">
     <template v-slot:body>
-      <div class="grid w-full grid-cols-2 gap-5">
+      <div class="grid w-full grid-cols-2 gap-5 px-10 py-10 bg-gray-700 rounded-lg">
       <div class="col-span-1">
-        <VMap style="height: 400px;">
+        <VMap >
           <VMapOsmTileLayer />
         </VMap>
       </div>
       <div class="w-full h-full col-span-1">
         <div>
-          <h1 class="font-[400] text-2xl leading-10 m-auto py-2 -tracking-tighter">Get in touch with us</h1>
+          <h1 class="font-[400] text-2xl leading-10 m-auto pb-2 -tracking-tighter">Liên hệ với chúng tôi..</h1>
         </div>
-        <div>
-          <span></span>
+        <div class="flex items-center justify-between gap-3 mb-3">
+          <img src="../../assets/images/default.png" alt="Ảnh đại diện" class="w-[50px] h-[50px] object-cover rounded-full mr-2 ml-5">
+          <p class="px-4 py-2 border-2 border-gray-200 border-solid rounded-md" :class="success ? 'text-green-500' : ''">
+            {{
+              success ? "Hi, I’m Amanda. Chúng tôi sẽ liên hệ trong thời gian sớm nhất" : "Hi, I’m Amanda. Need help? Use the form below or email me at hello@california_golfclub.org."
+            }}
+          </p>
         </div>
         <form class="w-full ">
           <!-- Form Payment -->
-          <div class="flex flex-col w-full gap-2 ">
+          <div class="flex flex-col w-full gap-2">
             <!-- Form full name. -->
             <div class="mb-4">
               <span class="text-sm"
@@ -39,10 +44,23 @@
                 name="address"
                 placeholder="abc@gmail.com"
                 v-model="state.email"
+                @keyup="onPressText($event, 'email')"
+                :classChild="v$.email.$error && error.emailError == true ? 'border-[2px] border-red-600 border-solid' : 'border-transparent'"
               />
-              <span v-if="v$.email.$error">
-              {{v$.value.email.email.$message}}
+              <!-- <span v-if="v$.email.$error" :class="v$.email.$error ? 'text-xs text-red-500' : 'hidden'"> {{ v$.email.$errors[0].$message }} </span>
+              <span v-if="v$.email.$error == false" >  </span> -->
+            </div>
+            <!-- Form phone. -->
+            <div class="mb-4">
+              <span class="text-sm"
+                >Điện thoại liên hệ <span class="text-lg align-middle text-brown">*</span>
               </span>
+              <Input
+                v-model="state.phone"
+                type="text"
+                name="phone"
+                placeholder="098xxxxxxx"
+              />
             </div>
             <!-- Form note detail. -->
             <div class="mb-4">
@@ -52,9 +70,6 @@
                 name="note"
                 v-model="state.message"
               />
-              <span v-if="v$.message.$error">
-              {{v$.value.message.message.$message}}
-              </span>
             </div>
           </div>
           <!-- Button submit order. -->
@@ -71,8 +86,8 @@
     </template>
   </BasePage>
 </template>
-<script setup>
-import { reactive, computed } from 'vue';
+<script>
+import { ref,reactive, computed } from 'vue';
 import useValidate from '@vuelidate/core'
 import { required, email, minLength, sameAs } from '@vuelidate/validators'
 import BasePage from '../auth/BasePage.vue';
@@ -80,34 +95,68 @@ import Button from '@/components/common/button/Button.vue';
 import Input from '@/components/common/input/Input.vue';
 import Textarea from '@/components/common/input/Textarea.vue';
 import { VMap, VMapOsmTileLayer } from 'vue-map-ui';
+export default {
+  name: "ContactPage",
+  components: {
+    BasePage,
+    Button,
+    Input,
+    Textarea,
+    VMap, 
+    VMapOsmTileLayer
+  },
+  setup() {
+    const error = reactive({
+      nameError: false,
+      emailError: false,
+      messageError: false
+    })
+    const state = reactive({
+      fullName: '',
+      email: '',
+      message: '',
+      phone: ''
+    });
+    const rules =  computed(() => {
+      return {
+        fullName: {required, minLength: minLength(6)},
+        email: {required, email},
+        phone: {required, }
+    }})
+    const success = ref(false);
+    const v$ = useValidate(rules, state);
 
-const state = reactive({
-  fullName: '',
-  email: '',
-  message: ''
-});
-const rules =  computed(() => {
-return {
-    fullName: {required, minLength: minLength(6)},
-  email: {required, email},
-  message: {required}
-  }})
-
-const v$ = useValidate(rules, state);
-
-const onSubmit = () => {
-  console.log(state);
-  console.log(v$.value.message);
-console.log(v$.value.email);
-console.log(v$.value.fullName);
-// console.log(v$.value.email.$error);
-console.log(v$.value.fullName.$error);
-  if (!v$.value.email.$error) {
-        // if ANY fail validation
-        alert('Form successfully submitted.')
-      } else {
-        alert('Form failed validation')
-      }
+  return {state, v$, error, success}
+},
+methods: {
+  onSubmit(){
+    this.v$.$validate()
+    if (!this.v$.$error) {
+          // if ANY fail validation
+          this.success = true;
+          console.log(this.state)
+        } else {
+          alert('Form failed validation')
+        }
+  },
+  onPressText(event, type) {
+    this.v$.$validate()
+    if (type == 'email') {
+      this.error.emailError = true;
+    } else {
+      this.error.emailError = false;
+    }
+  }
+}
 }
 </script>
-<style lang=""></style>
+<style lang="css" scoped>
+button {
+  background-color: #0c3247;
+  color: #17b1ea;
+}
+button:hover {
+  background-color: #124a68;
+  color: #17b1ea;
+}
+</style>
