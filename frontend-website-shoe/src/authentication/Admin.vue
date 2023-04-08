89db48cf426c2f767196d8f079d6a4c9449ec14c
mai.ngoc.dinh <ngocdinh2k1@gmail.com>
2023-04-09 01:57:10 +0700

    feature/frontend/#71: Update chat websocket

--- frontend-website-shoe/src/authentication/Admin.vue
+++ frontend-website-shoe/src/authentication/Admin.vue
@@ -6,43 +6,219 @@
     <div class="col-span-6 m-3 p-5 rounded-md bg-[#171B2D] shadow-md">
       <router-view />
     </div>
+    <div></div>
     <div
-      class="w-[350px] h-[400px] grid grid-rows-7 bottom-0 right-20 fixed bg-[#0c3247] z-10 text-white"
+      v-if="socket.connected == false"
+      @click="connect"
+      class="fixed flex items-center justify-end cursor-pointer right-2 bottom-3"
+    >
+      <span class="px-2 py-1 mr-2 text-xs bg-white rounded-[4px] w-fit">Liên hệ để hỗ trợ!</span>
+      <img
+        src="../assets/images/default.png"
+        class="w-[40px] h-[40px] object-cover rounded-full"
+        alt=""
+      />
+    </div>
+    <form
+      v-if="socket.connected == true"
+      class="w-[350px] h-[400px] grid grid-rows-7 bottom-0 right-20 fixed bg-gray-700 z-10 text-white"
+      method="post"
     >
       <div
-        class="flex items-center h-[50px] justify-between row-span-1 px-2 border-b-2 border-gray-500 border-solid"
+        class="flex items-center h-[50px] bg-gray-400 text-black justify-between row-span-1 px-2 border-b-2 border-gray-500 border-solid"
       >
-        <h2>Admin</h2>
-        <button class="translate-x-2">X</button>
-      </div>
-      <div class="flex flex-col h-full row-span-5 gap-3 px-2 py-2">
-        <div class="flex items-center justify-start gap-3">
+        <div class="flex items-center gap-2">
           <img
             src="../assets/images/default.png"
-            class="w-[50px] h-[50px] object-cover rounded-full"
+            class="w-[30px] h-[30px] object-cover rounded-full"
             alt=""
           />
-          <span class="px-3 border-gray-300 border-solid border-[1px] rounded-md"> text </span>
+          <h2>Khách hàng {{ message.sendMessage.messageTo }}</h2>
         </div>
-        <div class="flex items-center justify-end gap-3">
-          <span class="px-3 border-gray-300 border-solid border-[1px] rounded-md"> text </span>
-          <img
-            src="../assets/images/default.png"
-            class="w-[50px] h-[50px] object-cover rounded-full"
-            alt=""
-          />
+        <button
+          @click="disconnect"
+          class="text-white font-bold max-h-[36px] px-3 py-1 translate-x-4"
+        >
+          X
+        </button>
+      </div>
+      <div class="flex flex-col min-h-[300px] row-span-5 gap-3 px-2 overflow-y-scroll">
+        <div v-if="message.receivedMessagesDatabase.length != 0">
+          <div
+            v-for="(item, index) in message.receivedMessagesDatabase"
+            :key="index"
+            class="flex flex-col gap-3 px-2 py-1"
+          >
+            <div v-if="item.messageFrom != 'Admin'" class="flex items-center justify-start gap-3">
+              <img
+                src="../assets/images/default.png"
+                class="w-[40px] h-[40px] object-cover rounded-full"
+                alt=""
+              />
+              <span
+                class="px-3 border-gray-300 border-solid border-[1px] rounded-md max-w-[220px] gap-3 break-words"
+              >
+                {{ item.messageText }}
+              </span>
+              <strong class="text-[10px]">
+                {{ item.messageTime }}
+              </strong>
+            </div>
+            <div v-if="item.messageFrom == 'Admin'" class="flex items-center justify-end gap-3">
+              <strong class="text-[10px]">
+                {{ item.messageTime }}
+              </strong>
+              <span
+                class="px-3 border-gray-300 border-solid border-[1px] rounded-md max-w-[220px] gap-3 break-words"
+              >
+                {{ item.messageText }}
+              </span>
+              <img
+                src="../assets/images/default.png"
+                class="w-[40px] h-[40px] object-cover rounded-full"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+        <div
+          v-for="(response, index) in message.receivedMessages"
+          :key="index + message.receivedMessagesDatabase.length"
+          class="flex flex-col gap-3 px-2 -mt-1"
+        >
+          <div v-if="response.messageFrom != 'Admin'" class="flex items-center justify-start gap-3">
+            <img
+              src="../assets/images/default.png"
+              class="w-[40px] h-[40px] object-cover rounded-full"
+              alt=""
+            />
+            <span
+              class="px-3 border-gray-300 border-solid border-[1px] rounded-md max-w-[220px] gap-3 break-words"
+            >
+              {{ response.messageText }}
+            </span>
+            <strong class="text-[10px]">
+              {{ response.messageTime }}
+            </strong>
+          </div>
+          <div v-if="response.messageFrom == 'Admin'" class="flex items-center justify-end gap-3">
+            <strong class="text-[10px]">
+              {{ response.messageTime }}
+            </strong>
+            <span
+              class="px-3 border-gray-300 border-solid border-[1px] rounded-md max-w-[220px] gap-3 break-words"
+            >
+              {{ response.messageText }}
+            </span>
+            <img
+              src="../assets/images/default.png"
+              class="w-[40px] h-[40px] object-cover rounded-full"
+              alt=""
+            />
+          </div>
         </div>
       </div>
       <div class="flex items-end justify-between row-span-1">
-        <Input class="w-full h-[44px] rounded-none" />
-        <button class="w-[80px] text-sm m-0 py-3 rounded-none">Send</button>
+        <Input
+          v-model="message.sendMessage.messageText"
+          class="w-full h-[44px] rounded-none"
+        ></Input>
+        <button
+          @click.prevent="send"
+          id="send"
+          class="text-white font-bold w-[80px] text-sm m-0 py-3 rounded-none"
+        >
+          Send
+        </button>
       </div>
-    </div>
+    </form>
   </div>
 </template>
 <script setup>
+import { useCookies } from 'vue3-cookies'
+import { reactive, onMounted } from 'vue'
 import Input from '../components/common/input/Input.vue'
 import NavbarAdmin from '@/components/templates/NavbarAdmin.vue'
 import SidebarAdmin from '@/components/templates/SidebarAdmin.vue'
+import SockJS from 'sockjs-client'
+import Stomp from 'webstomp-client'
+import { globalCookiesConfig } from 'vue3-cookies'
+import ChatService from '@/stores/modules/ChatService'
+
+const login = reactive({
+  username: null,
+  password: null
+})
+const message = reactive({
+  receivedMessagesDatabase: [],
+  receivedMessages: [],
+  sendMessage: { chatId: 1, messageText: '', messageFrom: 'Admin', messageTo: '1' },
+  messageRequest: { messageFrom: '', messageTo: '' }
+})
+const socket = reactive({
+  socket: '',
+  stompClient: '',
+  connected: false
+})
+const { cookies } = useCookies()
+globalCookiesConfig({
+  expireTimes: '30d',
+  path: '/',
+  domain: '',
+  secure: true,
+  sameSite: 'None'
+})
+// cookies.set('user_cookie', globalCookiesConfig)
+onMounted(async () => {
+  await connect()
+  cookies.config('7d', '', '', true)
+})
+
+const send = () => {
+  console.log(message.receivedMessages)
+  if (socket.stompClient && socket.stompClient.connected) {
+    const sendMessage = message.sendMessage
+    if (login.username == null) {
+      message.sendMessage.messageTo = '1'
+    }
+    socket.stompClient.send('/app/chat', JSON.stringify(sendMessage), {})
+  }
+  message.sendMessage.messageText = ''
+}
+
+const connect = async () => {
+  message.messageRequest.messageFrom = 'Admin'
+  message.messageRequest.messageTo = '1'
+  await fetchAllMessage(message.messageRequest.messageFrom, message.messageRequest.messageTo)
+  socket.socket = new SockJS('http://localhost:8088/chat')
+  socket.stompClient = Stomp.over(socket.socket)
+  socket.stompClient.connect(
+    {},
+    (frame) => {
+      socket.connected = true
+      console.log(frame)
+      socket.stompClient.subscribe('/topic/messages', (tick) => {
+        message.receivedMessages.push(JSON.parse(tick.body))
+      })
+      console.log(message.receivedMessages)
+    },
+    (error) => {
+      console.log(error)
+      socket.connected = false
+    }
+  )
+}
+
+async function fetchAllMessage(messageFrom, messageTo) {
+  const res = await ChatService.getAll(messageFrom, messageTo)
+  message.receivedMessagesDatabase = res.data
+}
+
+const disconnect = () => {
+  if (socket.stompClient) {
+    socket.stompClient.disconnect()
+  }
+  socket.connected = false
+}
 </script>
 <style lang=""></style>
