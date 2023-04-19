a73e0bc498b59587b381e8e7750aa16b96bc3fdd
mai.ngoc.dinh <ngocdinh2k1@gmail.com>
2023-04-19 11:52:55 +0700

    feature/backend/#92: Send mail.

---
+++ backend-website-shoe/backend-website-shoe/src/main/java/com/graduationproject/backend/backendwebsiteshoe/model/MailResponseModel.java
@@ -0,0 +1,16 @@
+package com.graduationproject.backend.backendwebsiteshoe.model;
+
+import lombok.Data;
+
+/**
+ * Mail response dto.
+ *
+ * @author Mai Ngoc Dinh.
+ */
+@Data
+public class MailResponseModel {
+
+    private String message;
+
+    private Boolean status;
+}
